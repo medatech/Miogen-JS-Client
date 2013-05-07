f4b33103a3de56b21d2442a8ce14c31c300e9ca6
Martin Adams <martin@meda.co.uk>
2013-05-07 16:42:14 +0100

    Adding PUT and DELETE support

--- js/MiogenHttpClient.js
+++ js/MiogenHttpClient.js
@@ -10,68 +10,29 @@
         init: function () {},
 
         doGet: function (url, callback) {
-            $.ajax({
-                url: url,
-                headers: {
-                    'Accept': 'vnd.miogen+JSON'
-                },
-                type: 'GET',
-                contentType: 'text/json',
-                beforeSend: function (jqXHR) {
-                    jqXHR.setRequestHeader('Accept', 'vnd.miogen+JSON');
-                },
-                success: function (data, textStatus, jqXHR) {
-                    var doc = null;
-
-                    if (typeof (data) === 'string') {
-                        try {
-                            data = JSON.parse(data);
-                        } catch (ex) {}
-                    }
-
-                    if (typeof (data) !== 'object') {
-                        doc = new Miogen.MiogenDocument({
-                            collection: {
-                                href: null,
-                                version: 1.0,
-                                errors: [
-                                    data
-                                ]
-                            }
-                        });
-                    } else {
-                        doc = new Miogen.MiogenDocument(data);
-                    }
-
-                    callback(jqXHR.status, doc, jqXHR);
-
-                },
-                error: function (jqXHR, textStatus, errorThrown) {
-                    var doc = new Miogen.MiogenDocument({
-                        collection: {
-                            href: null,
-                            version: 1.0,
-                            errors: [
-                                errorThrown
-                            ]
-                        }
-                    });
-
-                    callback(jqXHR.status, doc, jqXHR);
-                }
-            });
+            this.callServer('GET', url, null, callback);
         },
 
         doPost: function (url, saveData, callback) {
-            $.ajax({
+            this.callServer('PUT', url, saveData, callback);
+        },
+
+        doPut: function (url, saveData, callback) {
+            this.callServer('PUT', url, saveData, callback);
+        },
+
+        doDelete: function (url, callback) {
+            this.callServer('DELETE', url, null, callback);
+        },
+
+        callServer: function (method, url, saveData, callback) {
+            var cfg = {
                 url: url,
                 headers: {
                     'Accept': 'vnd.miogen+JSON'
                 },
                 contentType: 'text/json',
-                type: 'POST',
-                processData: false,
-                data: JSON.stringify(saveData),
+                type: method,
                 beforeSend: function (jqXHR) {
                     jqXHR.setRequestHeader('Accept', 'vnd.miogen+JSON');
                 },
@@ -126,7 +87,15 @@
 
                     callback(jqXHR.status, doc, jqXHR);
                 }
-            });
+            };
+
+            if (method === 'POST' || method === 'PUT') {
+                cfg.processData = false;
+                cfg.data = JSON.stringify(saveData);
+            }
+
+
+            $.ajax(cfg);
         }
     }))();
 }());
